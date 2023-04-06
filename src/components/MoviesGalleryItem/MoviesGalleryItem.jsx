1677a88b2b6406c357fe57438e8dd17b4f3800ee
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-06 16:32:45 +0300

    Update MoviesGalleryItem.jsx

--- src/components/MoviesGalleryItem/MoviesGalleryItem.jsx
+++ src/components/MoviesGalleryItem/MoviesGalleryItem.jsx
@@ -3,13 +3,12 @@ import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { MovieListItem, MovieLink } from './MoviesGalleryItem.styled';
 
-const MoviesGalleryItem = ({ id, posterPath, overview, title, to }) => {
+const MoviesGalleryItem = ({ id, posterPath, overview, title, pathTo }) => {
   const location = useLocation();
-  console.log(location);
 
   return (
     <MovieListItem>
-      <MovieLink to={to}>
+      <MovieLink to={`${pathTo}${id}`} state={location}>
         <img src={posterPath} alt={overview} />
         <p>{title}</p>
       </MovieLink>
@@ -23,7 +22,7 @@ MoviesGalleryItem.propTypes = {
   posterPath: PropTypes.string.isRequired,
   overview: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired,
+  pathTo: PropTypes.string.isRequired,
 };
 
 export default MoviesGalleryItem;
