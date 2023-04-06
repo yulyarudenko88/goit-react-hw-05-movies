9ea01d19ee1ef508132b595f4442e0364b2ec5eb
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-06 17:22:53 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,22 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 import { fetchPopularMovies } from 'services/api';
-import Loader from 'components/Loader/Loader';
 import MainTitle from 'components/MainTitle/MainTitle';
 import MoviesGallery from 'components/MoviesGallery/MoviesGallery';
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-
     const getPopularMovies = async () => {
       try {
-        const data = await fetchPopularMovies();        
+        const data = await fetchPopularMovies();
         setPopularMovies(data);
-        setLoading(false);
       } catch (error) {
         toast.error(
           'Sorry for the inconvenience! Please try to use our service in a few minutes!'
@@ -29,17 +24,16 @@ const Home = () => {
 
   return (
     <>
-      {loading && <Loader />}
       {popularMovies === [] && (
         <MainTitle>
           Sorry for the inconvenience! Please try to use our service in a few
           minutes!
         </MainTitle>
       )}
-      {!loading && popularMovies.length > 0 && (
+      {popularMovies.length > 0 && (
         <>
           <MainTitle>Trending Today</MainTitle>
-          <MoviesGallery movies={popularMovies} pathTo={`movies/`}/>
+          <MoviesGallery movies={popularMovies} pathTo={`movies/`} />
         </>
       )}
     </>
